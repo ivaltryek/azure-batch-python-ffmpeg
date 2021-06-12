b428e5cdfa371e88dc11f02f410956c72f98dcf9
Meet Vasani <meet.vasani@ftxinfotech.com>
2021-06-12 19:35:53 +0530

    Updated task

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 def flip():
-    stream = ffmpeg.input('countdown.mov')
+    stream = ffmpeg.input('https://proplayaidev.blob.core.windows.net/pitchai-input/02B5CEE6-678C-40FC-9874-A99D8A42B522_b1b4e34b-3830-4e75-9ed5-0abd04898a50.mov')
     stream = ffmpeg.vflip(stream)
     stream = ffmpeg.output(stream, 'output.mp4')
     subprocess.call(f'echo {sys.argv[1]}')
