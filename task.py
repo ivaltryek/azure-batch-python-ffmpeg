5d5db29b610a2b4dae1514849e76b1fed25f9145
Meet Vasani <meet.vasani@ftxinfotech.com>
2021-06-12 19:54:39 +0530

    added logging

--- task.py
+++ task.py
@@ -1,16 +1,18 @@
 import ffmpeg
 import subprocess
 import sys
+import logging
+logging.basicConfig(level = logging.INFO)
+
 
 def flip():
-    print('Processing Started')
-    stream = ffmpeg.input('https://proplayaidev.blob.core.windows.net/pitchai-input/02B5CEE6-678C-40FC-9874-A99D8A42B522_b1b4e34b-3830-4e75-9ed5-0abd04898a50.mov')
+    logging.info('Processing Started')
+    stream = ffmpeg.input('https://proplayaidev.blob.core.windows.net/pitchai-input/countdown.mov')
     stream = ffmpeg.vflip(stream)
     stream = ffmpeg.output(stream, 'output.mp4')
-    subprocess.call(f'echo {sys.argv[1]}')
-    print('Hello World')
-    subprocess.call(['ls', '-la'])
+    logging.info(sys.argv[1])
+    logging.info('Hello World')
     ffmpeg.run(stream)
-    print('Finished')
+    logging.info('Finished')
     
 flip()
